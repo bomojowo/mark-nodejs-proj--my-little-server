92a805209e25ad65e4f4b2a6c44ca87d2490573d
Bukola Omojowo <bomojowo@gmail.com>
2021-07-19 11:58:28 +0100

    Added get requests to store history

--- src/server.ts
+++ src/server.ts
@@ -2,18 +2,22 @@ import express from "express";
 import ponyData from "../data/ponies.json";
 import { seasonOneEpisodes } from "./episodes";
 import { pickRandom } from "./random";
+import helloWorldData from "../data/helloWolrd.json"
 
 const app = express();
 const serverStartDate = new Date();
 let serverHitCount = 0;
+let activeRoute:string[] = []
 
 app.get("/", (req, res) => {
+  activeRoute.push(req.url)
   res.send(
     "This is the default path - and it isn't very interesting, sorry. \nTry visiting localhost:4000/creation-time, localhost:4000/current-time"
   );
 });
 
 app.get("/creation-time", (req, res) => {
+  activeRoute.push(req.url)
   res.json({
     message: `The server was started at ${serverStartDate.toTimeString()}`,
     utc: serverStartDate.toUTCString(),
@@ -23,7 +27,7 @@ app.get("/creation-time", (req, res) => {
 
 app.get("/current-time", (req, res) => {
   const dateOfRequestHandling = new Date();
-
+  activeRoute.push(req.url)
   res.json({
     message: `The current date is ${dateOfRequestHandling.toTimeString()}`,
     utc: dateOfRequestHandling.toUTCString(),
@@ -41,6 +45,7 @@ app.get("/hits", (req, res) => {
 });
 
 app.get("/hits-stealth", (req, res) => {
+  activeRoute.push(req.url)
   res.json({
     note: "Oooh, you ninja. We didn't count that hit.",
     currentTotal: serverHitCount,
@@ -49,12 +54,30 @@ app.get("/hits-stealth", (req, res) => {
 });
 
 app.get("/ponies", (req, res) => {
+  activeRoute.push(req.url)
   res.json({
     message: "Loaded dummy JSON data:",
     data: ponyData,
     countedAsHit: false,
   });
 });
+
+app.get("/history", (req, res) => {
+  activeRoute.push(req.url)
+  res.json({
+    message: 'Sites you have vistied',
+    routes: activeRoute
+  })
+})
+
+app.get("/ponies/random", (req, res) => {
+  const randomPonies = pickRandom(ponyData.members);
+  activeRoute.push(req.url)
+  res.json({
+    message: "random ponies",
+    data: randomPonies
+  })
+})
 
 app.get("/season-one", (req, res) => {
   res.json({
@@ -70,6 +93,13 @@ app.get("/season-one/random", (req, res) => {
     data: randomEpisode,
   });
 });
+
+app.get("/hello-world", (req, res) => {
+  res.json({
+    message: "this is the data",
+    data: helloWorldData
+  })
+})
 
 // using 4000 by convention, but could be changed
 const PORT_NUMBER = 4000;
